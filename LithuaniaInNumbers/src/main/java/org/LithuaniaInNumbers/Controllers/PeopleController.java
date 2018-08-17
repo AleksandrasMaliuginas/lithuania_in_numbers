0c5da8b0f110f48901cb64dc2daa70275fd587d6
Aleksandras <aleksandrasm348@gmail.com>
2018-08-17 16:01:01 +0300

    Updated file structure

--- LithuaniaInNumbers/src/main/java/org/LithuaniaInNumbers/Controllers/PeopleController.java
+++ LithuaniaInNumbers/src/main/java/org/LithuaniaInNumbers/Controllers/PeopleController.java
@@ -1,16 +1,25 @@
 package org.LithuaniaInNumbers.Controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+import java.util.ArrayList;
+import java.util.List;
+
+import org.LithuaniaInNumbers.DataModels.People.Population;
+
+@RestController
+@RequestMapping(value = "/people")
 public class PeopleController {
 	
-	@GetMapping("/hello")
-    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello";
-    }
+	@RequestMapping(value = "/population", method = RequestMethod.POST)
+	public List<Population> population(@RequestParam(name="name", required=false, defaultValue="World") String name) {
+		
+		List<Population> pop = new ArrayList<>();
+		pop.add(new Population());
+		
+		return pop;
+	}
 }
---
+++ LithuaniaInNumbers/src/main/java/org/LithuaniaInNumbers/DataModels/People/Population.java
@@ -0,0 +1,8 @@
+package org.LithuaniaInNumbers.DataModels.People;
+
+public class Population {
+	public int total = 54;
+	public int men;
+	public int women;
+	public int year;
+}
