f17d53fb772a58dc1bfa5b0d2ce1b9760493876a
Aleksandras <aleksandrasm348@gmail.com>
2018-09-27 22:11:18 +0300

    Add Density

---
+++ LithuaniaInNumbers/src/main/java/org/LithuaniaInNumbers/Models/People/Density.java
@@ -0,0 +1,11 @@
+package org.LithuaniaInNumbers.Models.People;
+
+public class Density {
+	public int year;
+	public float density;
+	
+	public Density(float density, int year) {
+		this.year = year;
+		this.density = density;
+	}
+}
